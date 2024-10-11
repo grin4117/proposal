871198c5ee7fc2a18bd632870e9ff9003fe7c3a8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-11 15:47:27 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/alarm.dart
+++ lib/custom_code/actions/alarm.dart
@@ -25,7 +25,7 @@ Future alarm(DateTime dateTime) async {
   final alarmSettings = AlarmSettings(
     id: 42,
     dateTime: dateTime,
-    assetAudioPath: 'assets/audios/Fallen.mp3',
+    assetAudioPath: 'assets/audios/Amnesia.mp4',
     loopAudio: true,
     vibrate: true,
     volume: 0.8,
